efd11a2853ba7effaa70c45f40494e8470262ba7
yangyulong@galaxy-future.com <Yangyulong908>
2021-11-19 15:34:46 +0800

    fix host

--- server/config/host.js
+++ server/config/host.js
@@ -1,5 +1,5 @@
-// const host = 'http://127.0.0.1:9090'
-const host = 'http://bridgx-api.internal.galaxy-future.org'
+const host = 'http://127.0.0.1:9090'
+// const host = 'http://bridgx-api.internal.galaxy-future.org'
 
 function getHost() {
   return host
